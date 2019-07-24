8f061a3059f81457141ce52cbcdf70340902032b
Misat11 <misa.turek1@gmail.com>
2019-07-24 21:55:32 +0200

    TNT Sheep AI

--- common/src/main/java/misat11/lib/nms/NMSUtils.java
+++ common/src/main/java/misat11/lib/nms/NMSUtils.java
@@ -1,9 +1,12 @@
 package misat11.lib.nms;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -93,6 +96,59 @@ public final class NMSUtils {
 		} catch (Throwable t) {
 
 		}
+	}
+
+	public static void changeTNTSheepAI(Mob mob, double speed, double follow) {
+		try {
+			Class<?> PathfinderGoalSelector = getNMSClass("PathfinderGoalSelector");
+			Class<?> PathfinderGoalMeleeAttack = getNMSClass("PathfinderGoalMeleeAttack");
+			Class<?> EntityCreature = getNMSClass("EntityCreature");
+			Class<?> PathfinderGoal = getNMSClass("PathfinderGoal");
+			Class<?> GenericAttributes = getNMSClass("GenericAttributes");
+			Class<?> IAttribute = getNMSClass("IAttribute");
+			Object methodProfiler = getMethodProfiler(mob.getWorld());
+			Object handler = mob.getClass().getMethod("getHandle").invoke(mob);
+			Object goalSelector = PathfinderGoalSelector.getConstructors()[0].newInstance(methodProfiler);
+			Object targetSelector = PathfinderGoalSelector.getConstructors()[0].newInstance(methodProfiler);
+			handler.getClass().getField("goalSelector").set(handler, goalSelector);
+			handler.getClass().getField("targetSelector").set(handler, targetSelector);
+			Object attack = PathfinderGoalMeleeAttack.getConstructor(EntityCreature, double.class, boolean.class)
+					.newInstance(handler, 1.0D, false);
+			goalSelector.getClass().getMethod("a", int.class, PathfinderGoal).invoke(goalSelector, 0, attack);
+			Object MOVEMENT_SPEED = GenericAttributes.getField("MOVEMENT_SPEED").get(null);
+			Object FOLLOW_RANGE = GenericAttributes.getField("FOLLOW_RANGE").get(null);
+			Object ATTACK_DAMAGE = GenericAttributes.getField("ATTACK_DAMAGE").get(null);
+			Object speedO = handler.getClass().getMethod("getAttributeInstance", IAttribute).invoke(handler,
+					MOVEMENT_SPEED);
+			speedO.getClass().getMethod("setValue", double.class).invoke(speedO, speed);
+			Object followO = handler.getClass().getMethod("getAttributeInstance", IAttribute).invoke(handler,
+					FOLLOW_RANGE);
+			followO.getClass().getMethod("setValue", double.class).invoke(followO, follow);
+			Object attrMap = handler.getClass().getMethod("getAttributeMap").invoke(handler);
+			Object attackO = attrMap.getClass().getMethod("b", IAttribute).invoke(attrMap,
+					ATTACK_DAMAGE);
+			attackO.getClass().getMethod("setValue", double.class).invoke(attackO, 0);
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	private static Object getMethodProfiler(World world) {
+		try {
+			Object handler = world.getClass().getMethod("getHandle").invoke(world);
+			Object methodProfiler;
+			try {
+				methodProfiler = handler.getClass().getMethod("getMethodProfiler").invoke(handler);
+			} catch (Throwable t) {
+				Field methodProfilerField = handler.getClass().getField("methodProfiler");
+				methodProfilerField.setAccessible(true);
+				methodProfiler = methodProfilerField.get(handler);
+			}
+			return methodProfiler;
+		} catch (Throwable t) {
+
+		}
+		return null;
 	}
 
 	public static void respawn(Plugin instance, Player player, long delay) {
